160f5dbf17e9f2bb25952ce73f208860c258c235
root <amjallan@gmail.com>
2018-05-30 20:50:49 -0400

    AutoScaling

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -300,3 +300,86 @@ resource "aws_cloudwatch_metric_alarm" "service_memory_average" {
   alarm_actions = ["${aws_appautoscaling_policy.up.arn}"]
   ok_actions    = ["${aws_appautoscaling_policy.down.arn}"]
 }
+
+/*====
+Auto Scaling para containers ECS
+======*/
+
+resource "aws_iam_role" "ecs_autoscale_role" {
+  name               = "${var.environment}_ecs_autoscale_role"
+  assume_role_policy = "${file("${path.module}/policies/ecs-autoscale-role.json")}"
+}
+resource "aws_iam_role_policy" "ecs_autoscale_role_policy" {
+  name   = "ecs_autoscale_role_policy"
+  policy = "${file("${path.module}/policies/ecs-autoscale-role-policy.json")}"
+  role   = "${aws_iam_role.ecs_autoscale_role.id}"
+}
+
+resource "aws_appautoscaling_target" "target" {
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.web.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  role_arn           = "${aws_iam_role.ecs_autoscale_role.arn}"
+  min_capacity       = 1
+  max_capacity       = 3
+}
+
+resource "aws_appautoscaling_policy" "up" {
+  name                    = "${var.environment}_scale_up"
+  service_namespace       = "ecs"
+  resource_id             = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.web.name}"
+  scalable_dimension      = "ecs:service:DesiredCount"
+
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment = 1
+    }
+  }
+
+  depends_on = ["aws_appautoscaling_target.target"]
+}
+
+resource "aws_appautoscaling_policy" "down" {
+  name                    = "${var.environment}_scale_down"
+  service_namespace       = "ecs"
+  resource_id             = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.web.name}"
+  scalable_dimension      = "ecs:service:DesiredCount"
+   step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment = -1
+    }
+  }
+
+  depends_on = ["aws_appautoscaling_target.target"]
+}
+
+/* metric used for auto scale */
+resource "aws_cloudwatch_metric_alarm" "service_memory_average" {
+  alarm_name          = "${var.environment}_projeto-testes_web_memory_utilization_avarege"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Avarege"
+  threshold           = "50"
+
+  dimensions {
+    ClusterName = "${aws_ecs_cluster.cluster.name}"
+    ServiceName = "${aws_ecs_service.web.name}"
+  }
+
+  alarm_actions = ["${aws_appautoscaling_policy.up.arn}"]
+  ok_actions    = ["${aws_appautoscaling_policy.down.arn}"]
+}
